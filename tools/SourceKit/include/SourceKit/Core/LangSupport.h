0de3aaba5459009c427f1dd1c29f039e8b7fa8af
Marc Rasi <marcrasi@google.com>
2019-07-02 16:44:03 -0700

    add shims for passing filesystem directly to langsupport

--- tools/SourceKit/include/SourceKit/Core/LangSupport.h
+++ tools/SourceKit/include/SourceKit/Core/LangSupport.h
@@ -816,6 +816,19 @@ public:
                                        ConformingMethodListConsumer &Consumer) = 0;
 
   virtual void getStatistics(StatisticsReceiver) = 0;
+
+  // SWIFT_ENABLE_TENSORFLOW
+  /// Tempoary shim for clients that want to pass the filesystem directly.
+  virtual void codeComplete(
+      llvm::MemoryBuffer *InputBuf, unsigned Offset,
+      CodeCompletionConsumer &Consumer, ArrayRef<const char *> Args,
+      llvm::IntrusiveRefCntPtr<clang::InMemoryOutputFileSystem> FS) = 0;
+
+  /// Tempoary shim for clients that want to pass the filesystem directly.
+  virtual void editorOpen(
+      StringRef Name, llvm::MemoryBuffer *Buf, EditorConsumer &Consumer,
+      ArrayRef<const char *> Args,
+      llvm::IntrusiveRefCntPtr<clang::InMemoryOutputFileSystem> FS) = 0;
 };
 } // namespace SourceKit
 
